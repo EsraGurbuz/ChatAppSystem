d050681c128164389f8391447e1b2debc87b5d3e
Esra Gürbüz <esragurbuze@gmail.com>
2026-03-20 20:20:23 +0300

    feat: add ChatMessage model and Newtonsoft.Json dependency in SharedLibrary

---
+++ ChatApp.Shared/ChatMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ChatApp.Shared
+{
+    // Encapsulation
+    public class ChatMessage
+    {
+        public string Sender { get; set; }
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public ChatMessage() { }
+
+        public ChatMessage(string sender, string message)
+        {
+            Sender = sender;
+            Message = message;
+            Timestamp = DateTime.Now;
+        }
+    }
+}
